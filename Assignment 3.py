07ecdc67a15ec36ae90f59047281a62e9c9687d5
Munyekiliz <elizakagwima@gmail.com>
2021-04-19 13:36:07 -0400

    Assignment 3

---
+++ Assignment 3.py
@@ -0,0 +1,16 @@
+#Enter Full Names
+names = input("Enter your first and last name ")
+fname = input("fname")#First Name
+lname = input("lname")#last Name
+fullnames= fname + "" + lname
+#Enter phone, email
+phone = input("phone")
+email = input("email")
+#price of a used car
+price = 10000
+down_payment = input("price")
+
+print('Names: ', names)
+print('Phone: ', phone)
+print('Email: ', email)
+print('Down payment: ', price)
